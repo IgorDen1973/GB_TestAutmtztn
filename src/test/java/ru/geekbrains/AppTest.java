58ff77b767d6d938aba04d9e5d0304893709f618
IgorDen1973 <garik_1@mail.ru>
2021-02-24 13:42:53 +0300

    Lesson #2 @ApacheMaven

---
+++ src/test/java/ru/geekbrains/AppTest.java
@@ -0,0 +1,22 @@
+package ru.geekbrains;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test; // импорт библиотеки
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println(">>Start test!\n");
+        assertTrue( true );
+        System.out.println("\n<<Finished test!");
+    }
+}
